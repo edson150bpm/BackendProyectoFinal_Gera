7a1dd694ba4c403593857c722a4701dadd062012
Polarking3503 <reynaga_45@hotmail.com>
2022-11-26 20:44:09 -0600

    agregando Configuracion Cors

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 const express = require ("express");
 const rutas = require ("./rutas/home.routes");
-
-//const cors = require("cors"); //Para darle seguridad cuando este montado
+const cors = require("cors"); //Para darle seguridad cuando este montado
 
 const app = express();
 //Configuracion
 
 app.set("port", 4000);
 app.set("host", "localhost");
+app.use(cors());
+
 
 app.use(express.json()); //Nuestra apliacion por medio de express va usar json
 
